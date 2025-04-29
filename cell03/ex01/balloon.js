4f82ac47a9d685a9d12ba50318b1849089cf3d6f
irinbear <txnnikaa@gmail.com>
2025-04-30 00:17:14 +0700

    edit ballon

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,26 @@
+let size = 200;
+const colors = ["red", "green", "blue"]
+let colorIndex = 0;
+const maxSize = 420;
+const originalSize = 200;
+const minSize = 200;
+
+function setSizeColor() {
+    size += 10;
+
+    if (size > maxSize) {
+        size = originalSize;
+    }
+
+    const div = document.getElementById("balloon")
+    div.style.width = size + "px";
+    div.style.height = size + "px";
+
+    colorIndex = (colorIndex + 1) % colors.length;
+    div.style.backgroundColor = colors[colorIndex];
+
+};
+
+function decreaseSize(){
+
+}
