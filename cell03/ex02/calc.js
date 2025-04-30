7f2c7d936e28e44e2c1988502bbab243a3b8b9fe
irinbear01 <irinb3ar@gmail.com>
2025-04-30 15:10:45 +0700

    ex2.1

---
+++ cell03/ex02/calc.js
@@ -0,0 +1,4 @@
+let screen = document.querySelector(".screen");
+let buttons = document.querySelectorAll(".button")
+let clear = document.querySelector(".clear")
+let equal = document.querySelector(".equal")
