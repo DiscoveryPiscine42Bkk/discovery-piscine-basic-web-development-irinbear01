5199017421bebcd8fbcfd7e55757990b5280ffb8
irinbear01 <irinb3ar@gmail.com>
2025-04-30 22:12:29 +0700

    Delete etc./ex02 directory

--- etc./ex02/calc.js
+++
@@ -1,4 +0,0 @@
-let screen = document.querySelector(".screen");
-let buttons = document.querySelectorAll(".button")
-let clear = document.querySelector(".clear")
-let equal = document.querySelector(".equal")
