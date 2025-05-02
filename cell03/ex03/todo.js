74355d23329bc6cf8299f373feb98f0034bc6a2b
irinbear <txnnikaa@gmail.com>
2025-05-02 09:59:05 +0700

    cookie

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -2,26 +2,50 @@ const ft_list = document.getElementById("ft_list");
 
 function addTodo() {
     const text = prompt("What you want to do:");
-    if (text !== " ") {
+    if (text && text.trim() !== "") {
+
         const newDiv = document.createElement("div")
         newDiv.innerText = text
         newDiv.addEventListener("click", removeTodo);
-        newDiv.prepend(ft_list);
-        
+        ft_list.prepend(newDiv);
+        saveToCookie();
     }
 }
 
 function removeTodo() {
     const confirmDelete = confirm("Do you want to delete?")
     if (confirmDelete) {
-        
+        event.target.remove();
+        saveToCookie();  
     }
 }
 
 function saveToCookie() {
-
+    const todos = [];
+    for (let child of ft_list.children) {
+        if (child.tagName.toLowerCase() === "div") {
+            todos.push(child.innerText);
+        }
+    }
+    const todoString = JSON.stringify(todos);
+    document.cookie = "todos=" + encodeURIComponent(todoString) + ";path=/";
 }
 
 function loadFromCookie() {
-
+    const cookies = document.cookie;
+    const match = cookies.match(/(?:^|;\s*)todos=([^;]*)/);
+    if (match) {
+        const todoString = decodeURIComponent(match[1]);
+        const todos = JSON.parse(todoString);
+        for (let text of todos) {
+            const newDiv = document.createElement("div");
+            newDiv.innerText = text;
+            newDiv.addEventListener("click", removeTodo);
+            ft_list.prepend(newDiv);
+        }
+    }
 }
+
+window.onload = function () {
+    loadFromCookie();
+};
